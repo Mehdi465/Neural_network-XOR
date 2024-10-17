b2beb02c192dc7217c17de7c6a7963b2df0c2157
Mehdi465 <mehdi.fellahi06@gmail.com>
2024-10-17 23:43:11 +0200

    commit

--- C++/simple_NN.cpp
+++ C++/simple_NN.cpp
@@ -26,6 +26,10 @@ void DenseLayer::forward(std::vector<double> input){
 
 }
 
+void DenseLayer::backward(std::vector<double> output_gradient){
+
+}
+
 /**
  * class Activation Layer implementation
  */
@@ -34,7 +38,39 @@ ActivationLayer::ActivationLayer() = default;
 
 ActivationLayer::ActivationLayer(int size){};
 
+void ActivationLayer::forward(std::vector<double> input){
 
+}
+
+void ActivationLayer::backward(std::vector<double> output_gradient) {
+
+}
+
+/**
+ * class Network implementation
+ */
+
+Network::Network() = default;
+
+Network::Network(int num_layers){
+    this->num_layer = num_layer;
+}
+
+void Network::forward_network(){
+
+}
+
+void Network::backward_network(){
+
+}
+
+void Network::train_network(std::vector<std::vector<double>> inputs,std::vector<std::vector<double>> outputs,
+        int num_samples, int input_size,int output_size, int epochs){
+
+}
+
+// MAIN //
+//////////
 
 int main(){
 
